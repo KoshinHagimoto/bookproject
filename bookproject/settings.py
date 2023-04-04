8b2587ed343bf18d29c33a9797489e2812a8b4d2
KoshinHagimoto <koshin.hagimoto@gmail.com>
2023-04-04 21:56:12 +0900

    redeploy

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://django-practice.herokuapp.com/']
+ALLOWED_HOSTS = ['https://django-practice.herokuapp.com']
 
 
 # Application definition
